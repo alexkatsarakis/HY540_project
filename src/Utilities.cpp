ef3eca44800ccfde43ba4aa6bd9ad5820e2f3fa2
papamano <papadomano@gmail.com>
2020-04-06 19:43:39 +0300

    Utility functions

---
+++ src/Utilities.cpp
@@ -0,0 +1,15 @@
+#include "Utilities.h"
+
+#include <cmath>
+#include <limits>
+
+bool Utilities::IsZero(double num) {
+    unsigned val = static_cast<unsigned>(num * 100000.0f);
+    return val == 0;
+}
+
+bool Utilities::IsInt(double num) {
+    double flooredValue = std::floor(std::abs(num));
+    double epsilon = std::numeric_limits<double>::epsilon();
+    return (std::abs(num - flooredValue) < epsilon);
+}
---
+++ src/Utilities.h
@@ -0,0 +1,12 @@
+#ifndef _UTILITIES_H_
+#define _UTILITIES_H_
+
+namespace Utilities {
+
+    bool IsZero(double);
+
+    bool IsInt(double);
+
+}
+
+#endif
