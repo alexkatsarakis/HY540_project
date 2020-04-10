44e36881b7e58f030bb9eac237edfa70af5ee753
papamano <papadomano@gmail.com>
2020-04-10 16:00:51 +0300

    Implemented more library functions

--- src/LibraryFunctions.cpp
+++ src/LibraryFunctions.cpp
@@ -10,7 +10,9 @@ unsigned whitespace = 0;
 
 #define WHITESPACE_STEP 4
 
-#define RETURN_NULL env.Set("$retval", NilTypeValue::Nil); return
+#define RETVAL_RESERVED_FIELD "$retval"
+
+#define RETURN_NIL do { env.Set(RETVAL_RESERVED_FIELD, NilTypeValue::Nil); return; } while(false);
 
 const Value * GetArgument(Object & env, unsigned argNo, const std::string & optArgName = "") {
     assert(env.IsValid());
@@ -36,12 +38,12 @@ void PrintValue(std::ostream & stream, const Value * value) {
     else if (value->IsLibraryFunction()) stream << "[Library Function \"" << value->ToLibraryFunctionId() << "\"]" << std::endl;
     else if (value->IsNativePtr()) stream << "[Pointer " << value->ToNativeTypeId() << "]" << std::endl;
     else if (value->IsObject()) {
-        stream << "[Object] [ " << std::endl;
+        stream << "[Object] [" << std::endl;
         Object * o = value->ToObject_NoConst();
 
         whitespace += WHITESPACE_STEP;
         o->Visit([&stream](const Value &key, const Value &val) {
-            if (key.IsString()) stream << std::setw(whitespace) << "\"" << key.ToString() << "\" : ";
+            if (key.IsString()) stream << std::setw(whitespace) << '"' << key.ToString() << "\" : ";
             else if (key.IsNumber()) stream << std::setw(whitespace) << key.ToNumber() << " : ";
             else assert(false);
 
@@ -62,5 +64,39 @@ void LibFunc::Print(Object & env) {
         PrintValue(std::cout, value);
     }
 
-    RETURN_NULL;
+    RETURN_NIL;
+}
+
+void LibFunc::Typeof(Object & env) {
+    assert(env.IsValid());
+    const Value * value = GetArgument(env, 0);
+    env.Set(RETVAL_RESERVED_FIELD, value->GetTypeToString());
+}
+
+void LibFunc::ObjectKeys(Object & env) {
+    assert(env.IsValid());
+
+    const Value * value = GetArgument(env, 0);
+    if (!value->IsObject()) RETURN_NIL; /* TODISCUSS: Perhaps a runtime error? */
+
+    const Object * obj = value->ToObject();
+    Object * table = new Object();
+    table->IncreaseRefCounter();
+    unsigned i = 0;
+
+    obj->Visit([table, &i](const Value &key, const Value &val) {
+        table->Set(i++, key);
+    });
+
+    env.Set(RETVAL_RESERVED_FIELD, table);
+}
+
+void LibFunc::ObjectSize(Object & env) {
+    assert(env.IsValid());
+
+    const Value * value = GetArgument(env, 0);
+    if (!value->IsObject()) RETURN_NIL; /* TODISCUSS: Perhaps a runtime error? */
+
+    const Object * obj = value->ToObject();
+    env.Set(RETVAL_RESERVED_FIELD, static_cast<double>(obj->GetTotal()));
 }
